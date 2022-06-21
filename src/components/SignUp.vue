1ecef850565b63b8c4b59c5b25c8557b95a48dcd
silviawolves <silviacanepa.w@gmail.com>
2022-06-21 18:33:45 +0200

    sing-up completo

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <div class="sign-up">
-            <div class="container">sign up bar</div>
+            <div class="container d-flex justify-content-between align-items-center">
+                <button class="btn btn-outline-primary rounded-0 text-uppercase text-white">Sign-up now!</button>
+
+                <div class="links text-primary text-uppercase fw-bold">
+                    <span class="pe-2">Follow Us</span>
+                    <a href="javascript.void(0)">
+                        <img v-for="(img, i) in socialArray" :key="i" :src="`img/${img.image}.png`" :alt="`Icona social di ${img.name}`" class="ps-3">
+                    </a>
+                </div>
+
+            </div>
         </div>
     </div>
 </template>
@@ -9,6 +19,32 @@
 <script>
 export default {
     name: 'SignUp',
+    data() {
+        return {
+            socialArray: [
+        {
+            name: 'Facebook',
+            image: 'footer-facebook',
+        },
+        {
+            name: 'Twitter',
+            image: 'footer-twitter',
+        },
+        {
+            name: 'Youtube',
+            image: 'footer-youtube',
+        },
+        {
+            name: 'Pinterest',
+            image: 'footer-pinterest',
+        },
+        {
+            name: 'Periscope',
+            image: 'footer-periscope',
+        },
+    ]
+        }
+    }
 }
 </script>
 
