5b0fec04b67748807402d32b275fd0de5b2f4644
silviawolves <silviacanepa.w@gmail.com>
2022-06-22 14:24:48 +0200

    layout fumetti

--- src/App.vue
+++ src/App.vue
@@ -35,9 +35,6 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  //height: 100vh;
-  background-color: black;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  height: 100vh;
 }
 </style>
---
+++ src/components/ContainerFumetti.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="my-card">
+            <div class="card-img">
+                <img :src="imgUrl" :alt="`Immagine di ${imgText}`">
+            </div>
+            
+            <div class="card-title">{{ imgText }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ContainerFumetti',
+    props: {
+        imgUrl: String,
+        imgText: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.my-card {
+
+    .card-img {
+        overflow: hidden;
+        aspect-ratio: 1/1;
+    }
+
+    .card-title {
+        text-transform: uppercase;
+        font-family: Arial, Helvetica, sans-serif;
+        font-weight: normal;
+        font-size: .7rem;
+        padding: 1rem 0;
+    }
+}
+</style>
